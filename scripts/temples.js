04fc99c651d0f950708ceb13e6ebf5f0eb82381b
VinKage4 <vsilvadesousa@byupathway.edu>
2025-03-28 20:54:51 -0600

    custom button menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,3 +8,10 @@ const lastMod = document.lastModified;
 document.getElementById("lastmodified").textContent = `Last Modification: ${lastMod}`;
 
 // menu toggle hamburger
+const hambutton = document.querySelector('#hambutton');
+const navmenu = document.querySelector('#navmenu');
+
+hambutton.addEventListener('click', () => {
+  navmenu.classList.toggle('open');
+  hambutton.classList.toggle('open');
+});
